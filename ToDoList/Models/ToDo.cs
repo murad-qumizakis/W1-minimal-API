f3c5c7e0131ff8851ca991f89cedc43dc8eacb8e
Murad Qumizakis <muradqumizakis@gmail.com>
2023-04-17 19:20:21 -0700

    week1-minimal-api

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ToDoList.Models
+{
+	public class ToDo
+	{
+		public string? Id { get; set; }
+		public string? name { get; set; }
+		public string? createdAt { get; set; }
+		public string? content{ get; set; }
+		public override string ToString() => JsonSerializer.Serialize<ToDo>(this);
+
+    }
+}
+
